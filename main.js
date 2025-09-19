c6ee4b785a69ae38a285e5a74ef4d58c5b17057c
Piezel <michalradon06@gmail.com>
2025-09-19 17:14:17 +0200

    a

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
 const themebutton = document.querySelector("#themebutton")
 
 themebutton.addEventListener("click",()=>{
-    let tab = document.querySelectorAll(".light")
-    tab.forEach((el,i)=>{
-        el.classList.toggle("light")
-        el.classList.toggle("dark")
-    })
+    const body = document.body
+    const themeAnimation = [
+        {transform: "rotateY(0)"},
+        {transform: "rotateY(360deg)"}
+    ]
+    const themeAnimationTiming = {
+        duration: 500,
+        iterations: 1,
+    }
+    if(body.classList.contains("light")) {
+        let tab = document.querySelectorAll(".light")
+        let menubutton = document.querySelectorAll(".menulight")
+        tab.forEach((el,i)=>{
+            el.classList.remove("light")
+            el.classList.add("dark")
+        })
+        menubutton.forEach((el,i)=>{
+            el.classList.remove("menulight")
+            el.classList.add("menudark")
+        })
+        themebutton.innerHTML = '<i class="fa-regular fa-moon fa-xl"></i>'
+        themebutton.animate(themeAnimation,themeAnimationTiming)
+    } else {
+        let tab = document.querySelectorAll(".dark")
+        let menubutton = document.querySelectorAll(".menudark")
+        tab.forEach((el,i)=>{
+            el.classList.remove("dark")
+            el.classList.add("light")
+        })
+        menubutton.forEach((el,i)=>{
+            el.classList.remove("menudark")
+            el.classList.add("menulight")
+        })
+        themebutton.innerHTML = '<i class="fa-regular fa-sun fa-xl"></i>'
+        themebutton.animate(themeAnimation,themeAnimationTiming)
+    }
 })
